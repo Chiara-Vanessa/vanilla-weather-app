15e9bca2d97586f50a03b2d91bbeec7983f3bb03
Chiara-Vanessa <chiara.rubin@bluewin.ch>
2023-09-07 16:34:21 +0200

    Fixed the forecast and added some styling

--- src/app.js
+++ src/app.js
@@ -68,8 +68,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2471d7af42c5308f65f51a71386e8d0d";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "1dbf926d3b4417bf379db7043bec1047";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
